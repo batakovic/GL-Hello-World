d863c266b0dd721c253723c8881995b1db636269
Mladen Batakovic <mladen.batakovic@propulsionapp.com>
2013-04-05 12:30:29 +0200

    database

--- application/models/Person.php
+++ application/models/Person.php
@@ -32,22 +32,39 @@ class Person
     private $phone;
     private $date;
     
+    /**
+     * Construct
+     */
     public function __construct() 
     {
         $dbAdapter = Zend_Registry::getInstance()->dbAdapter;
         $dbAdapter->setFetchMode(Zend_Db::FETCH_OBJ);
         $this->dbAdapter = $dbAdapter;
     }
-    
+    /**
+     * Method for setting values
+     * @param type $name
+     * @param type $value
+     */
     public function __set($name, $value)
     {
         $this->$name = $value;
     }
+    
+    /**
+     * Method return object attribute
+     * @param type $name
+     * @return type
+     */
     public function __get($name)
     {
         return $this->$name;
     }
     
+    /**
+     * Return all persons in person table
+     * @return type
+     */
     public static function getAllPersons()
     {
         $dbAdapter = Zend_Registry::getInstance()->dbAdapter;
@@ -61,11 +78,19 @@ class Person
        
     }
     
+    /**
+     * Insert new Person in table
+     * @param type $data
+     */
     public function insert($data)
     {
         $this->dbAdapter->insert('person', $data);
     }
     
+    /**
+     * Delete persons
+     * @param type $data - array()
+     */
     public function delete($data = array())
     {
         foreach ($data as $value) {
